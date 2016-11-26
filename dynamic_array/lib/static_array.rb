f22a485b99231ea14e758251759a57b023034679
fan222 <sfwahahaha222@gmail.com>
2016-11-26 13:07:27 -0800

    finish static array

--- dynamic_array/lib/static_array.rb
+++ dynamic_array/lib/static_array.rb
@@ -1,16 +1,31 @@
 # This class just dumbs down a regular Array to be staticly sized.
 class StaticArray
   def initialize(length)
+    @store = Array.new(length)
   end
 
   # O(1)
   def [](index)
+    validate!(index)
+    @store[index]
   end
 
   # O(1)
   def []=(index, value)
+    validate!(index)
+    @store[index] = value
+  end
+
+  def length
+    @store.length
   end
 
   protected
   attr_accessor :store
+
+  private
+
+  def validate!(ind)
+    raise "Overflow error" unless ind.between?(0, length - 1)
+  end
 end
